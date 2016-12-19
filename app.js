7ff393a6b86395f5fdb857f05d5a5ab33c12f022
Franck Aragão <fjunior.aragao@gmail.com>
2016-12-19 18:07:03 -0300

    configuration of app to port of heroku

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-app.set('port', 3000);
+// seleciona process.env.PORT, caso exista variavel de ambiente (heroku)
+app.set('port', (process.env.PORT || 3000));
 
 app.use(express.static(path.join(__dirname, 'src')));
 
