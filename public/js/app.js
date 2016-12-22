266081a92249fb0ca98a1808b12892200b471987
Franck Aragão <fjunior.aragao@gmail.com>
2016-12-22 01:08:29 -0300

    update configuration to access offline

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,18 @@
 var aqtApp = angular.module("aqtApp", ['ngRoute']);
+
+
+/**
+Service Worker register
+**/
+if ('serviceWorker' in navigator && (window.location.protocol === 'https:' || window.location.hostname === 'localhost')) {
+  
+  navigator.serviceWorker.register('sw.js', {scope: './'
+  }).then(function(registration) {
+    if (typeof registration.update == 'function') {
+      registration.update();
+    }
+  }).catch(function(e) {
+    console.error('Error during service worker registration:', e);
+  });
+  
+}
