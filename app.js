162b1164dc03f3e440740516e0f7f6745692294c
Franck Aragão <fjunior.aragao@gmail.com>
2017-01-07 13:34:20 -0300

    disabled service worker of app

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-// seleciona process.env.PORT, caso exista variavel de ambiente (heroku)
 app.set('port', (process.env.PORT || 3000));
 
 app.use(express.static(path.join(__dirname, 'src')));
--- src/app/aqt.module.js
+++ src/app/aqt.module.js
@@ -1,17 +1,15 @@
 var aqtApp = angular.module("aqtApp", ['ngRoute', 'aqtDirectivies', 'ngTagsInput']);
 
-/**
-Service Worker register
-**/
-if ('serviceWorker' in navigator && (window.location.protocol === 'https:' || window.location.hostname === 'localhost')) {
 
-  navigator.serviceWorker.register('sw.js', {scope: './'
-  }).then(function(registration) {
-    if (typeof registration.update == 'function') {
-      registration.update();
-    }
-  }).catch(function(e) {
-    console.error('Error during service worker registration:', e);
-  });
-
-};
+// if ('serviceWorker' in navigator && (window.location.protocol === 'https:' || window.location.hostname === 'localhost')) {
+//
+//   navigator.serviceWorker.register('sw.js', {scope: './'
+//   }).then(function(registration) {
+//     if (typeof registration.update == 'function') {
+//       registration.update();
+//     }
+//   }).catch(function(e) {
+//     console.error('Error during service worker registration:', e);
+//   });
+//
+// };
