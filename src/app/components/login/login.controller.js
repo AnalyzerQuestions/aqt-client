005735e0ef3fd244142432cee23e775f34587ca8
Franck Aragão <fjunior.aragao@gmail.com>
2017-02-05 23:05:00 -0300

    only heroku test 7

--- src/app/components/login/login.controller.js
+++ src/app/components/login/login.controller.js
@@ -14,17 +14,11 @@ angular.module("components").controller("loginController", function($location, a
     initSO();
 
     vm.login = function() {
-        console.log('tenta fazer login...');
-        SE.authenticate({
-            success: function(data) {
-                console.log('Success :', data);
-                $location.path('/main');
-            },
-            error: function(data) {
-                console.log('Error: ', data);
-            },
-            networkUsers: true
-        });
+        $("#somediv").load('https://stackexchange.com/oauth/dialog?client_id=7061&scope=no_expiry&redirect_uri=https://appif.herokuapp.com/#/main')
+            .dialog({
+                modal: true
+            });
+
     }
 
     function initSO() {
