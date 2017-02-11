b07d24358143971c5e73b140c925827a9ba2dff6
Franck Aragão <fjunior.aragao@gmail.com>
2017-02-11 11:21:26 -0300

    verify error

--- src/app/components/login/login.controller.js
+++ src/app/components/login/login.controller.js
@@ -42,6 +42,7 @@ angular.module("components").controller("loginController", function($location, a
 
     function registrationSOPt(networkUsers) {
         networkUsers.forEach(function(network) {
+            console.log(network);
             if (network.site_url === 'http://pt.stackoverflow.com') {
                 return network;
             }
