a352e35a9d32397fa5a9b06f3a1a970d5e8933ef
Franck Aragão <fjunior.aragao@gmail.com>
2017-02-11 13:47:28 -0300

    get user

--- src/app/common/side-nav.component.js
+++ src/app/common/side-nav.component.js
@@ -1,16 +1,32 @@
 angular.module("common").component('sideNav', {
 
-    bindings: {
-        name: '<'
-    },
-
     templateUrl: './side-nav.component.html',
 
-    controller: function() {
+    controller: function($location, aqtValue) {
 
         $(".button-collapse").sideNav();
-        console.log(localStorage.getItem("userSO").soPt);
 
+        var storage = localStorage.getItem("userSO");
+
+        if (storage) {
+            console.log(storage.soPt);
+
+            $http({
+                method: 'jsonp',
+                url: aqtValue.so.api + "users/me",
+                params: {
+                    key: aqtValue.so.key,
+                    access_token: storage.accessToken,
+                    site: aqtValue.so.site,
+                }
+            }).success(function(data, status, header, config) {
+                console.log('success user...', data);
+            }).error(function(data, status, header, config) {
+                console.log('error user...');
+            });
+
+        } else {
+            $location.path('/');
+        }
     }
-
 });
