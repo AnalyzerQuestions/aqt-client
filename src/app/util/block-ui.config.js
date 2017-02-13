6906fd468c986d1ba4bed29e4b2465f7854ce665
Franck Aragão <fjunior.aragao@gmail.com>
2017-02-12 22:28:14 -0300

    using block ui

--- src/app/util/block-ui.config.js
+++ src/app/util/block-ui.config.js
@@ -1,4 +1,5 @@
 angular.module("aqtApp").config(function(blockUIConfig) {
     //  blockUIConfig.template = '<div id=""><i class="fa fa-refresh fa-spin"></i></div>';
+    blockUIConfig.autoBlock = false;
 
 });
